47678714a3b4e7390af77e9efc11304cf9f2f2cc
iivanov21 <iivanov21@hotmail.com>
2021-07-08 08:55:50 +0000

    II:Test even nums

--- programs/even_numbers.py
+++ programs/even_numbers.py
@@ -1,12 +1,15 @@
 def even_digits(digit_one,digit_two):
     even_list=[]
-    for i in range(digit_one,digit_two):
-        if i % 2==0: even_list.append(i)
-    return even_list
+    for i in range(digit_one,digit_two+1):
+      for j in map(int, str(i)):
+          if j % 2: break
+      else : even_list.append(str(i))
+    return ",".join(even_list)
+   
 
 if __name__ == "__main__":
     n1=int(input("Please enter the first number:"))
     n2=int(input("Please enter the second number:"))
 
-    print(even_digits(n1,n2+1))
+    print(even_digits(n1,n2))
 
